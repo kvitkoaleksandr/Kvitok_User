27b508d92a65a29bcd65748ac3e72ef432e36e86
kvitkoaleksandr <kvitok-993@mail.ru>
2025-08-19 23:14:01 +0300

    Добавл поля в сущность User

--- src/main/java/com/yourname/booktracker/entity/User.java
+++ src/main/java/com/yourname/booktracker/entity/User.java
@@ -1,5 +1,67 @@
 package com.yourname.booktracker.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 64, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", length = 64, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone", length = 32, unique = true)
+    private String phone;
+
+    @Column(name = "password", length = 128, nullable = false)
+    private String password;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    @Column(name = "about_me", length = 4096)
+    private String aboutMe;
+
+    @Column(name = "city", length = 64)
+    private String city;
+
+    @Column(name = "experience")
+    private Integer experience;
+
+    @CreationTimestamp
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "followers")
+    private List<User>  followers;
+
+    @Column(name = "followees")
+    private List<User> followees;
+
+    @Column(name = "mentees")
+    private List<User> mentees;
+
+    @Column(name = "mentors")
+    private List<User> mentors;
 }
